478d930c55831636f076b0fb9d4f6501f35ab532
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-08-23 11:39:10 +0200

    Added Todo list

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
+/*
+* Todo:
+*   * välj ett ord
+*     - statisk
+*     - user input?
+*     - från en lista?
+*     - från en fil?
+*     - från en hemsida
+*   * använder väljer en bokstav
+*   * finns bokstaven i ordet?
+*       - var är bokstaven?
+*       - visa bokstaven
+*       - bli av med försök
+*   * loopa tills
+*       - ordet är helt
+*       - inga liv
+*   *
+
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
